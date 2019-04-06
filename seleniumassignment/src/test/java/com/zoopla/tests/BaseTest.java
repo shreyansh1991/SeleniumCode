e429cd191410f31bb27293d1c224f5e613c0e53d
Your Name <you@example.com>
2019-04-06 15:33:04 +0530

    Final Changes.

--- seleniumassignment/src/test/java/com/zoopla/tests/BaseTest.java
+++ seleniumassignment/src/test/java/com/zoopla/tests/BaseTest.java
@@ -4,9 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import com.zoopla.utilities.ExcelReader;
